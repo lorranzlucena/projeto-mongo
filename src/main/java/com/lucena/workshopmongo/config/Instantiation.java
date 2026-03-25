20bf9c4a4e4812070ccf24f21996449e99979c5d
lorranz <lorranz.lucena.sousa@gmail.com>
2026-03-25 16:51:33 -0300

    criando entity post com user aninhado

--- src/main/java/com/lucena/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucena/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.lucena.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.lucena.workshopmongo.domain.Post;
 import com.lucena.workshopmongo.domain.User;
+import com.lucena.workshopmongo.repository.PostRepository;
 import com.lucena.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,18 +19,28 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria", "maria@gmail.com");
 		User alex = new User(null, "Isla ", "alex@gmail.com");
 		User bob = new User(null, "lorranz ", "bob@gmail.com");
 
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		Post post1 = new Post(null,sdf.parse("21/03/2018"),"partiu curitiba"," CHEGAMOS EM CURITBA!!!!",maria );
+		Post post2 = new Post(null,sdf.parse("23/03/2018"),"bom dia","acordei feliz",maria);
 		
+		
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
