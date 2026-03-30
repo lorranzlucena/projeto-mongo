842e638346d2e1f8986af132ce2df806b41ad27c
lorranz <lorranz.lucena.sousa@gmail.com>
2026-03-30 14:47:29 -0300

    consulta simples com @query

--- src/main/java/com/lucena/workshopmongo/Repository/PostRepository.java
+++ src/main/java/com/lucena/workshopmongo/Repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.lucena.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lucena.workshopmongo.domain.Post;
@@ -13,5 +14,9 @@ public interface PostRepository  extends MongoRepository<Post, String>{
 	
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
-		
+	
+	@Query("{'title': { $regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
+	
+
 }
