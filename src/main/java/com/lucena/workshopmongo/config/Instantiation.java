7353bd054783511970a2670ed749302dde70b036
lorranz <lorranz.lucena.sousa@gmail.com>
2026-03-30 14:06:59 -0300

    acrescentando comentario aos posts

--- src/main/java/com/lucena/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucena/workshopmongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.lucena.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lucena.workshopmongo.domain.Post;
 import com.lucena.workshopmongo.domain.User;
 import com.lucena.workshopmongo.dto.AuthorDTO;
+import com.lucena.workshopmongo.dto.CommentDTO;
 import com.lucena.workshopmongo.repository.PostRepository;
 import com.lucena.workshopmongo.repository.UserRepository;
 
@@ -37,16 +39,25 @@ public class Instantiation implements CommandLineRunner {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		Post post1 = new Post(null,sdf.parse("21/03/2018"),"partiu curitiba"," CHEGAMOS EM CURITBA!!!!", new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"),"bom dia","acordei feliz", new AuthorDTO(maria));
 		
-		
+		CommentDTO c1 = new CommentDTO("boa viagem", sdf.parse("02/02/2020"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("aprovetei a viagem", sdf.parse("02/02/2020"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("tenha um otimo dia!", sdf.parse("02/02/2020"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post1.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		
 		maria.getPost().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
+		
+		
+		
 	}
 
 }
--- src/main/java/com/lucena/workshopmongo/domain/Post.java
+++ src/main/java/com/lucena/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.lucena.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.lucena.workshopmongo.dto.AuthorDTO;
+import com.lucena.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,7 +24,10 @@ public class Post implements Serializable {
 	private String body;
 
 	private AuthorDTO author;
-
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
+	
 	public Post() {
 		super();
 	}
@@ -90,6 +96,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 }
